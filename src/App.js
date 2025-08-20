1b5de3ddbe2100a826d571d04f11b98a85585869
Wembembo <timothy.a.fitz@gmail.com>
2025-08-20 07:47:41 +0100

    Started migrating from version 1.1, built in JS only, to building using React.

--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 class="header_title">Tekken 8 Quick Matchup Guide</h1>
+        <p class="header_subtitle">A quick referencing tool for matchups, aimed at beginners and commentators</p>
+        <p>The information here is subject to change depending on the latest patch. I'll be trying to keep on top of everything, but this will need some time to update as patches are released. </p>
+        <p>You can find the latest patch notes <a href="https://www.tekken-official.jp/tekken_news/?p=1500">here</a>. Version 2.02.00 is the current version of the game</p>
       </header>
     </div>
   );
